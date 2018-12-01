831ef546d72fce288e6deb8f736cb50e41386dec
Michael Loughry <miclo@microsoft.com>
2018-11-30 21:19:19 -0800

    Code cleanup

--- src/1/index.ts
+++ src/1/index.ts
@@ -2,26 +2,17 @@ import { Solution } from "../common/SolutionFile";
 import path from 'path';
 import fs from 'mz/fs';
 
-export default async function(): Promise<Solution> {
+export default async function (): Promise<Solution> {
     let input = await fs.readFile(path.resolve(__dirname, 'input.txt'), 'utf8');
 
     const seenFrequencies = new Set<number>([0]);
 
     const inputNumbers = input
-    .split('\n')
-    .map(i => parseInt(i, 10));
+        .split('\n')
+        .map(i => parseInt(i, 10));
 
     const part1 = inputNumbers
         .reduce((acc, i) => acc + i, 0);
-            // const frequency = acc + i;
-
-            // if (part2 === Number.MAX_VALUE && seenFrequencies.has(frequency)) {
-            //     part2 = frequency;
-            // }
-
-            // seenFrequencies.add(frequency);
-
-            // return frequency;
 
     let part2 = 0;
     for (let i = 0; ; i = (i + 1) % inputNumbers.length) {
@@ -33,5 +24,5 @@ export default async function(): Promise<Solution> {
     }
 
 
-    return {part1, part2};
+    return { part1, part2 };
 }
