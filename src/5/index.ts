4ea78535bdc544b5617352b43180058f488d5680
Michael Loughry <miclo@microsoft.com>
2018-12-04 21:13:46 -0800

    Day 5

--- src/5/index.ts
+++ src/5/index.ts
@@ -5,8 +5,34 @@ import fs from 'mz/fs';
 export default async function (): Promise<Solution> {
     const input = await fs.readFile(path.resolve(__dirname, 'input.txt'), 'utf8');
 
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const annihilationRegex = new RegExp(
+        alphabet
+            .split('')
+            .map(l => `${l}${l.toUpperCase()}|${l.toUpperCase()}${l}`)
+            .join('|'),
+            'g'
+    );
+
+    const part1 = collapsePolymer(input);
+
+    let part2 = part1;
+    for(const l of alphabet) {
+        part2 = Math.min(
+            part2,
+            collapsePolymer(input.replace(new RegExp(`[${l}${l.toUpperCase()}]`, 'g'), ''))
+        );
+    }
+
     return {
-        // part1,
-        // part2,
+        part1,
+        part2,
     };
+
+    function collapsePolymer(collapsedString: string) {
+        for (let lastLength = 0; collapsedString.length !== lastLength; collapsedString = collapsedString.replace(annihilationRegex, '')) {
+            lastLength = collapsedString.length;
+        }
+        return collapsedString.length;
+    }
 }
