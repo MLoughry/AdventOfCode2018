e0f65737bc1218794483f7b6b4a7633203dfbd9d
Michael Loughry <miclo@microsoft.com>
2018-12-06 08:47:28 -0800

    WIP - Day 6

--- src/6/index.ts
+++ src/6/index.ts
@@ -2,11 +2,51 @@ import { Solution } from "../common/SolutionFile";
 import path from 'path';
 import fs from 'mz/fs';
 
+interface Point {
+    x: number;
+    y: number;
+}
+
+interface Rect {
+    x: [number, number];
+    y: [number, number];
+}
+
+interface LinearEquation {
+    slope: number;
+    offset: number;
+}
+
 export default async function (): Promise<Solution> {
     const input = await fs.readFile(path.resolve(__dirname, 'input.txt'), 'utf8');
+
+    const points: Point[] = input
+        .split('\n')
+        .map(line => {
+            const [x, y] = line.split(', ').map(i => parseInt(i))
+            return {x, y};
+        });
+
+    const bounds: Rect = points
+        .reduce<Rect>(({x, y}, {x: px, y: py}) => {
+            return {
+                x: [
+                    Math.min(px, x[0]),
+                    Math.max(px, x[1]),
+                ],
+                y: [
+                    Math.min(py, y[0]),
+                    Math.max(py, y[1]),
+                ],
+            }
+        }, {x: [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER ], y: [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]});
 
     return {
         // part1,
         // part2,
     };
+}
+
+function findSlopeBetweenPoints(a: Point, b: Point) {
+
 }
