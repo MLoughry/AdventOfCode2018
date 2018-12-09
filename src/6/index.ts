a9cbcba429e8bab143963488b9005f04905da6a7
Michael Loughry <miclo@microsoft.com>
2018-12-09 12:23:02 -0800

    Day 6

--- src/6/index.ts
+++ src/6/index.ts
@@ -24,11 +24,11 @@ export default async function (): Promise<Solution> {
         .split('\n')
         .map(line => {
             const [x, y] = line.split(', ').map(i => parseInt(i))
-            return {x, y};
+            return { x, y };
         });
 
-    const bounds: Rect = points
-        .reduce<Rect>(({x, y}, {x: px, y: py}) => {
+    const pointBounds: Rect = points
+        .reduce<Rect>(({ x, y }, { x: px, y: py }) => {
             return {
                 x: [
                     Math.min(px, x[0]),
@@ -39,14 +39,77 @@ export default async function (): Promise<Solution> {
                     Math.max(py, y[1]),
                 ],
             }
-        }, {x: [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER ], y: [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]});
+        }, { x: [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER], y: [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER] });
+
+    const part1 = calculatePart1(pointBounds, points);
+
+    const maximumAggregateDistance = 10000;
+
+    let part2 = 0;
+    for (let x = pointBounds.x[0]; x <= pointBounds.x[1]; x++) {
+        for (let y = pointBounds.y[0]; y <= pointBounds.y[1]; y++) {
+            let aggregateDistance = 0;
+            for (const p of points) {
+                aggregateDistance += getManhattanDistance(p, {x, y});
+            }
+
+            if (aggregateDistance < maximumAggregateDistance) {
+                part2++;
+            }
+        }
+    }
 
     return {
-        // part1,
-        // part2,
+        part1,
+        part2,
     };
 }
 
-function findSlopeBetweenPoints(a: Point, b: Point) {
+function calculatePart1(pointBounds: Rect, points: Point[]) {
+    const searchBounds: Rect = {
+        x: [2 * pointBounds.x[0] - pointBounds.x[1], 2 * pointBounds.x[1] - pointBounds.x[0]],
+        y: [2 * pointBounds.y[0] - pointBounds.y[1], 2 * pointBounds.y[1] - pointBounds.y[0]],
+    };
+    const pointDomainSizes = new Map<Point | null, number>();
+    for (let x = searchBounds.x[0]; x <= searchBounds.x[1]; x++) {
+        for (let y = searchBounds.y[0]; y <= searchBounds.y[1]; y++) {
+            let closestPoint = findClosestPointTo(points, { x, y });
+            if (closestPoint) {
+                pointDomainSizes.set(closestPoint, (pointDomainSizes.get(closestPoint) || 0) + 1);
+            }
+        }
+    }
+    // Eliminate any points that have infinite domains, by searching one level out from the searchBounds
+    for (let x = searchBounds.x[0]; x <= searchBounds.x[1]; x++) {
+        pointDomainSizes.delete(findClosestPointTo(points, { x, y: searchBounds.y[0] - 1}));
+        pointDomainSizes.delete(findClosestPointTo(points, { x, y: searchBounds.y[1] - 1}));
+    }
+    for (let y = searchBounds.y[0]; y <= searchBounds.y[1]; y++) {
+        pointDomainSizes.delete(findClosestPointTo(points, { x: searchBounds.x[0] - 1, y}));
+        pointDomainSizes.delete(findClosestPointTo(points, { x: searchBounds.x[1] - 1, y}));
+    }
+    const part1 = [...pointDomainSizes.values()]
+        .reduce((acc, i) => Math.max(acc, i), 0);
+    return part1;
+}
 
+function findClosestPointTo(searchPoints: Point[], domainPoint: Point) {
+    let closestPoint: Point | null = null;
+    let closestDistance: number = Number.MAX_SAFE_INTEGER;
+
+    for (const p of searchPoints) {
+        const distance = getManhattanDistance(p, domainPoint);
+        if (distance < closestDistance) {
+            closestPoint = p;
+            closestDistance = distance;
+        } else if (distance === closestDistance) {
+            closestPoint = null;
+        }
+    }
+
+    return closestPoint;
+}
+
+function getManhattanDistance({x: x1, y: y1}: Point, {x: x2, y: y2}: Point): number {
+    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 }
